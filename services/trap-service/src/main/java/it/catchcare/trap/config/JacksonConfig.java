197157d5274c20b212f48bd8162602b6fbcdb30d
Giuseppe Pile <pile.giuseppe@gmail.com>
2025-09-22 15:59:07 +0200

    Add JacksonConfig class to configure ObjectMapper

---
+++ services/trap-service/src/main/java/it/catchcare/trap/config/JacksonConfig.java
@@ -0,0 +1,23 @@
+package it.catchcare.trap.config;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Configuration class for Jackson ObjectMapper.
+ * This class is responsible for creating and configuring the ObjectMapper bean used for JSON serialization and deserialization.
+ * Needed because this service does not have the spring-boot-starter-web dependency, which would automatically configure it.
+ */
+@Configuration
+public class JacksonConfig {
+
+    @Bean
+    public ObjectMapper objectMapper() {
+        ObjectMapper om = new ObjectMapper();
+        // Register the JavaTimeModule to handle java.time.* data types (dependency jackson-datatype-jsr310)
+        om.registerModule(new JavaTimeModule());
+        return om;
+    }
+}
